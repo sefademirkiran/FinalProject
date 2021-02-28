e48729d556ed33f46e17aedd65c41f0e4a0b2d34
sefademirkiran <64711131+sefademirkiran@users.noreply.github.com>
2021-02-28 15:11:16 +0300

    27.02.2021 Ders Sonu

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,8 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -9,10 +11,23 @@ namespace Business.Constants
     {
         public static string ProductAdded = "Ürün Eklendi ";
         public static string ProductNameInvalid = "Ürün ismi geçersiz.. ";
-        public static string MaintenanceTime ="Sistem Bakımda";
-        public static string ProductListed ="Ürünler Listelendi";
-        public static string ProductCountOfCategoryError="Maksimum 10 Adet ürün girilebilir. ";
-        public static string ProductNameAlreadyExists="Bu isimde zaten başka bir ürün var.";
-        public static string CategoryLimitExceded="Kategori limiti aşıldı. ";
+        public static string MaintenanceTime = "Sistem Bakımda";
+        public static string ProductListed = "Ürünler Listelendi";
+        public static string ProductCountOfCategoryError = "Maksimum 10 Adet ürün girilebilir. ";
+        public static string ProductNameAlreadyExists = "Bu isimde zaten başka bir ürün var.";
+        public static string CategoryLimitExceded = "Kategori limiti aşıldı. ";
+        public static string AuthorizationDenied = "Yetkiniz Yok";
+
+        public static string UserRegistered = "Kayıt Oldu.";
+
+        public static string UserNotFound = "Kullancı Bulunamadı";
+
+        public static string PasswordError = "Paralo Yanlış";
+
+        public static string SuccessfulLogin = "Başarılı Giriş";
+
+        public static string UserAlreadyExists = "Kullanıcı Mevcut";
+
+        public static string AccessTokenCreated = "Token Oluşturuldu";
     }
 }
